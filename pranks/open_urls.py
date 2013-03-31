b77e558541b944a31912bfa96a0cb84c34082f35
Harry Stern <hcs@synonymbun.com>
2013-03-31 15:31:51 -0400

    added url opening prank. add more funny links

---
+++ pranks/open_urls.py
@@ -0,0 +1,24 @@
+import subprocess
+import random
+
+urls = [
+    ("http://www.youtube.com/watch?v=wDajqW561KM", "Congratulations!"),
+    ("http://www.youtube.com/watch?v=yzC4hFK5P3g", "PONPONPON"),
+    ("http://www.youtube.com/watch?v=LkCNJRfSZBU", "LEEEEEEEEEROOOOOOOOOOOOY"),
+    ("http://www.youtube.com/watch?v=wvsboPUjrGc", "DEVELOPERS DEVELOPERS DEVELOPERS"),
+    ("https://www.smore.com/clippy-js", "Have some Clippy with your Clippy"),
+    ("http://en.wikipedia.org/wiki/Special:Random", "Opening a random wikipedia page"),
+    ("http://www.youtube.com/watch?v=xuPSIbABYVU", "Jazz hands!"),
+    ("http://www.reddit.com/r/mylittlepony", "Do you like ponies?"),    
+]
+
+def run_prank():
+    """Randomly opens funny urls, displays message"""
+
+    url_command = "xdg-open {url}"
+
+    url = random.choice(urls)
+
+    url_p = subprocess.Popen(url_command.format(url=url[0]) , shell=True, stdout=subprocess.PIPE)
+
+    return (True, url[1]) 
